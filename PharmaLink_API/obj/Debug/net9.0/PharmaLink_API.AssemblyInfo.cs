f335d662f42cc66149fb4f9abc00760c09c7bd96
Hosam Ayoub <hosamayoub50@gmail.com>
2025-07-21 22:01:25 +0300

    Update project paths and version information
    - Updated `AssemblyInformationalVersionAttribute` in `PharmaLink_API.AssemblyInfo.cs`.
    - Changed project paths and user directories in `PharmaLink_API.csproj.nuget.dgspec.json` and `project.assets.json` to reflect new GitHub location.
    - Updated `project.nuget.cache` with new `dgSpecHash` and project file path.
    - Modified `runtimeIdentifierGraphPath` in relevant files from "9.0.205" to "9.0.201".
    - Noted binary differences in `PharmaLink_API.assets.cache`.

--- PharmaLink_API/obj/Debug/net9.0/PharmaLink_API.AssemblyInfo.cs
+++ PharmaLink_API/obj/Debug/net9.0/PharmaLink_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8932568068e3fa975a854201ccf825cf8c22de85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb4790f4b17bba1de086cbc6742fd7c3b0a3b720")]
 [assembly: System.Reflection.AssemblyProductAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
