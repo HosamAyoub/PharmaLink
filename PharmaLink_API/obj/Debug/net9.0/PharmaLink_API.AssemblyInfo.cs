0f6be4af50052b5a1f41bf42e93c348a06094043
Mahmoud Ayman <109273486+Mahmoud1780@users.noreply.github.com>
2025-07-21 23:52:07 +0300

    before merging

--- PharmaLink_API/obj/Debug/net9.0/PharmaLink_API.AssemblyInfo.cs
+++ PharmaLink_API/obj/Debug/net9.0/PharmaLink_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53ade3e0a569d10c79d031c03c5c2564bcd45fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc85bb87f3492a21d19efe5d64a0f16145332c54")]
 [assembly: System.Reflection.AssemblyProductAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
