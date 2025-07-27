9806b0ef9d40e75ed1a222bb63f77318a61a1253
Mariem Sameh <maryamsameh61@gmail.com>
2025-07-27 23:06:30 +0300

    before merge to mariem

--- PharmaLink_API/obj/Debug/net9.0/PharmaLink_API.AssemblyInfo.cs
+++ PharmaLink_API/obj/Debug/net9.0/PharmaLink_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c681b9d04a7652de6e5d0a2f00a749760c8f693")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d56e7b338bc3268ed84e25ebe067c756e50401fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
