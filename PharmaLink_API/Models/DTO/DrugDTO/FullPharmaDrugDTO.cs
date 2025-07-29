2180267feac441233a8c93042737a2439491a027
Mahmoud22620 <114251031+Mahmoud22620@users.noreply.github.com>
2025-07-29 20:52:34 +0300

    Edit Drug_Add Profiles Favorite Controller

--- PharmaLink_API/Models/DTO/DrugDTO/FullPharmaDrugDTO.cs
+++ PharmaLink_API/Models/DTO/DrugDTO/FullPharmaDrugDTO.cs
@@ -15,6 +15,7 @@ namespace PharmaLink_API.Models.DTO.DrugDTO
     {
         public int Pharma_Id { get; set; }
         public string Pharma_Name { get; set; }
+        public string Pharma_Address { get; set; }
         public string Pharma_Location { get; set; }
         public decimal Price { get; set; }
         public int QuantityAvailable { get; set; }
