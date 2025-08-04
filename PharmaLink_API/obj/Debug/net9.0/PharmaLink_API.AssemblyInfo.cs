a60d8869d623021f307bd364eebe6136195dbe01
Mahmoud Ayman <109273486+Mahmoud1780@users.noreply.github.com>
2025-08-04 09:07:16 +0300

    After adding PharmacyAnalysis

--- PharmaLink_API/obj/Debug/net9.0/PharmaLink_API.AssemblyInfo.cs
+++ PharmaLink_API/obj/Debug/net9.0/PharmaLink_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9431a4ec9fc1680fa42f1d2f9383c76e7fe8e24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+855e0bc157e2a97bdb968155be082eaafc2718e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
