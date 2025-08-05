53a8205ba20b146a7596f21900b44e5db39659e3
Hosam Ayoub <hosamayoub50@gmail.com>
2025-08-05 21:00:33 +0300

    All tests worked

--- PharmaLink_API/obj/Debug/net9.0/PharmaLink_API.AssemblyInfo.cs
+++ PharmaLink_API/obj/Debug/net9.0/PharmaLink_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6699d66b71f0b3ba27eda1d36d795f1b73a27c51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb80beda07afd364b6dfdb2f13fdae3aad3a0e95")]
 [assembly: System.Reflection.AssemblyProductAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
