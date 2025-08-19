e625eb23e3e2df7469b24c5f14bd72ad467bbf1f
apdomustafa <apdomustafa2000@gmail.com>
2025-08-19 20:47:11 +0300

    configuration instead of hardcoded values, enhancing security and flexibility. Added a new `EmailSettings` section in `appsettings.json` for easier management of email settings.

--- PharmaLink_API/Services/EmailSender.cs
+++ PharmaLink_API/Services/EmailSender.cs
@@ -8,11 +8,17 @@ namespace PharmaLink_API.Services
 {
     public class EmailSender : IEmailSender
     {
+        private readonly IConfiguration configuration;
+
+        public EmailSender(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         public async Task sendEmailAsync(string email, string subject, string message)
         {
-            var apiKey = "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";
+            var apiKey = configuration["EmailSettings:key"];
             var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("apdom765@gmail.com","PharmaLink");
+            var from = new EmailAddress(configuration["EmailSettings:fromEmail"], configuration["EmailSettings:fromName"]);
             var to = new EmailAddress(email);
             var msg = MailHelper.CreateSingleEmail(from, to, subject, null, message);
             var response = await client.SendEmailAsync(msg);
