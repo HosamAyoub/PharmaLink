be3ed144fb3b2c274a39ed8b2196901d3dc7b599
Mahmoud Ayman <109273486+Mahmoud1780@users.noreply.github.com>
2025-08-26 05:00:14 +0300

    update pharmacy reg

--- PharmaLink_API/obj/Debug/net9.0/PharmaLink_API.AssemblyInfo.cs
+++ PharmaLink_API/obj/Debug/net9.0/PharmaLink_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05ac0367b547a353b63b290fbeaea8ea68890311")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f35a605f1ceaf04d15993abf503ccbb0e075c8c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PharmaLink_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
